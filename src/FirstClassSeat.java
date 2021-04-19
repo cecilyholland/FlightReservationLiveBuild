895c8a0bb6cdecbee17e4b635e47cf588e84e0ab
cecilyholland <80843091+cecilyholland@users.noreply.github.com>
2021-04-19 12:46:14 -0400

    javadoc update

--- src/FirstClassSeat.java
+++ src/FirstClassSeat.java
@@ -1,15 +1,23 @@
-
+/**
+ * extra fee class for first class flight
+ * 
+ */
 public class FirstClassSeat extends FlightSeat {
     
     
-    
+ /**
+ * calculates extra for first class
+ * 
+ */
     public FirstClassSeat()
     {
         classType = "First Class";
         extraCharge = 220.78;
        
     }
-
+/**
+* print statement clarifying for user what class they chose
+*/
     public void classGreeting()
     {
         System.out.println("Wow, you're fancy! Thanks for flying first class");
